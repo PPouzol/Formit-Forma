edd0c1bf32541f79579db761b93a9e6ca8bcaf70
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-02-22 15:52:49 -0500

    Added proper versioning handling according to documentation

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
+  build: {
+    outDir: './v0_1'
+  },
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
         {
           src: './manifest.json',
-          dest: '.'
-        },
-        {
-          src: './versions.json',
           dest: '.'
         }
       ]
