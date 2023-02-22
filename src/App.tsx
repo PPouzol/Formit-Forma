7fe8ea1d807dc8d2363d31bcddddc6369fa1e6fa
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-02-22 11:21:14 -0500

    Added first version of Login page using Typescript / Vite / React

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import './App.css'
+import AuthService from "./services/auth.service";
+import IUser from './types/user.type';
+
+import Login from "./components/login.component";
+
+type Props = {};
+
+type State = {
+  currentUser: IUser | undefined
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      currentUser: undefined,
+    };
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      currentUser: undefined,
+    });
+  }
+  
+  render() {
+    const { currentUser } = this.state;
+
+    return (
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
+    );
+  }
+}
+
+export default App
---
+++ src/services/auth.service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "http://app.spacemaker.api/auth/";
+
+class AuthService {
+  login(username: string, password: string) {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  getCurrentUser() {
+    const userStr = localStorage.getItem("user");
+    if (userStr) return JSON.parse(userStr);
+
+    return null;
+  }
+}
+
+export default new AuthService();
