1c39c65f5e82a998c05117269c1a2861615d3166
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-03-03 13:26:46 -0500

    Updates

--- plugin_formit.js
+++ plugin_formit.js
@@ -17,3 +17,48 @@ FormitPlugin.CloseDialog = function(){
     //close parent dialog window
     FormIt.CloseDialogBox();
 }
+
+FormitPlugin.EnsureInstances = function() {
+    const aBodiesAndMeshes = []
+    const aOtherForInstance = []
+    debugger
+    const topLevels = WSM.APIGetAllNonOwnedReadOnly(MAIN_HISTORY_ID)
+    for (i=0; i < topLevels.length; i++) {
+      debugger
+      nObjID = topLevels[i];
+      const nType = WSM.APIGetObjectTypeReadOnly(MAIN_HISTORY_ID, nObjID)
+      if (nType === WSM.nBodyType || nType === WSM.nMeshType) {
+        aBodiesAndMeshes.push(nObjID)
+      } else if (
+        nType === WSM.nFaceType ||
+        nType === WSM.nEdgeType ||
+        nType === WSM.nVertexType ||
+        nType === WSM.nLineMeshType ||
+        nType === WSM.nPointMeshType
+      ) {
+        aOtherForInstance.push(nObjID)
+      }
+    }
+    
+    debugger
+    if (aBodiesAndMeshes.length > 0 || aOtherForInstance.length > 0) {
+      debugger
+      FormIt.UndoManagement.BeginState()
+  
+      // Create one instance per each body and mesh.
+      for (const nObjID of aBodiesAndMeshes) {
+        debugger
+        WSM.Utils.CreateAlignedAndCenteredGroup(MAIN_HISTORY_ID, [nObjID])
+      }
+  
+      if (aOtherForInstance.length > 0) {
+        debugger
+        // Create one instance for all the remaining stuff
+        WSM.Utils.CreateAlignedAndCenteredGroup(MAIN_HISTORY_ID, aOtherForInstance)
+      }
+  
+      debugger
+      FormIt.UndoManagement.EndState("Move toplevels to instances")
+    }
+    debugger
+  }
