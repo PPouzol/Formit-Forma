0b6a1cb5f5241c78a1c2a19eb790e43a02374d1b
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-03-03 10:18:03 -0500

    Updates

--- plugin_formit.js
+++ plugin_formit.js
@@ -18,16 +18,16 @@ FormitPlugin.CloseDialog = function(){
     FormIt.CloseDialogBox();
 }
 
-FormitPlugin.EnsureInstances = function() {
+FormitPlugin.EnsureInstances = function(){
     debugger
-    
-  const aBodiesAndMeshes = []
-  const aOtherForInstance = []
-  const topLevels = WSM.APIGetAllNonOwnedReadOnly(MAIN_HISTORY_ID)
+  const MAIN_HISTORY_ID = 0;
+  const aBodiesAndMeshes = [];
+  const aOtherForInstance = [];
+  const topLevels = WSM.APIGetAllNonOwnedReadOnly(MAIN_HISTORY_ID);
   for (const nObjID of topLevels) {
-    const nType = WSM.APIGetObjectTypeReadOnly(MAIN_HISTORY_ID, nObjID)
+    const nType = WSM.APIGetObjectTypeReadOnly(MAIN_HISTORY_ID, nObjID);
     if (nType === WSM.nBodyType || nType === WSM.nMeshType) {
-      aBodiesAndMeshes.push(nObjID)
+      aBodiesAndMeshes.push(nObjID);
     } else if (
       nType === WSM.nFaceType ||
       nType === WSM.nEdgeType ||
@@ -35,23 +35,23 @@ FormitPlugin.EnsureInstances = function() {
       nType === WSM.nLineMeshType ||
       nType === WSM.nPointMeshType
     ) {
-      aOtherForInstance.push(nObjID)
+      aOtherForInstance.push(nObjID);
     }
   }
   
   if (aBodiesAndMeshes.length > 0 || aOtherForInstance.length > 0) {
-    FormIt.UndoManagement.BeginState()
+    FormIt.UndoManagement.BeginState();
 
     // Create one instance per each body and mesh.
     for (const nObjID of aBodiesAndMeshes) {
-      WSM.Utils.CreateAlignedAndCenteredGroup(MAIN_HISTORY_ID, [nObjID])
+      WSM.Utils.CreateAlignedAndCenteredGroup(MAIN_HISTORY_ID, [nObjID]);
     }
 
     if (aOtherForInstance.length > 0) {
       // Create one instance for all the remaining stuff
-      WSM.Utils.CreateAlignedAndCenteredGroup(MAIN_HISTORY_ID, aOtherForInstance)
+      WSM.Utils.CreateAlignedAndCenteredGroup(MAIN_HISTORY_ID, aOtherForInstance);
     }
 
-    FormIt.UndoManagement.EndState("Move toplevels to instances")
+    FormIt.UndoManagement.EndState("Move toplevels to instances");
   }
 }
