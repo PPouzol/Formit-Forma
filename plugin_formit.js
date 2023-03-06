58293af880e0e9d9fb417e183778762e9b83dae2
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-03-06 14:48:04 -0500

    Updates

--- plugin_formit.js
+++ plugin_formit.js
@@ -17,5 +17,28 @@ FormitPlugin.ShowDialog = function(){
 
 FormitPlugin.CloseDialog = function(){
     //close parent dialog window
+    WSM.Utils.ComputeGeometryFromLevels
     FormIt.CloseDialogBox();
+}
+
+FormitPlugin.FillTypesArrays = function(){
+  const aBodiesAndMeshes = []
+  const aOtherForInstance = []
+  const topLevels = WSM.APIGetAllNonOwnedReadOnly(MAIN_HISTORY_ID)
+  for (i=0; i < topLevels.length; i++) {
+    var nObjID = topLevels[i];
+    const nType = WSM.APIGetObjectTypeReadOnly(MAIN_HISTORY_ID, nObjID)
+    if (nType === WSM.nBodyType || nType === WSM.nMeshType) {
+      aBodiesAndMeshes.push(nObjID)
+    } else if (
+      nType === WSM.nFaceType ||
+      nType === WSM.nEdgeType ||
+      nType === WSM.nVertexType ||
+      nType === WSM.nLineMeshType ||
+      nType === WSM.nPointMeshType
+    ) {
+      aOtherForInstance.push(nObjID)
+    }
+  }
+  return {aBodiesAndMeshes, aOtherForInstance};
 }
