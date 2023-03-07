dcabbbc8546ad698e2236f7201400ec04805e146
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-03-07 11:24:09 -0500

    Updates

--- plugin_formit.js
+++ plugin_formit.js
@@ -33,7 +33,6 @@ FormitPlugin.ComputeGeometryFromLevels = function(objectId) {
 }
 
 FormitPlugin.Multiply = function(args) {
-  debugger
   var offsetTransf3d = args[0];
   var transf3d = args[1];
   return WSM.Transf3d.Multiply(offsetTransf3d, transf3d);
