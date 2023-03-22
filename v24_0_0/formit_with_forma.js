6b0ab4a810fac1be1bba746d21b76190bdfbe4af
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-03-22 14:18:53 -0400

    Try manual push for addin plugin addition

---
+++ v24_0_0/formit_with_forma.js
@@ -0,0 +1,19 @@
+window.FormIt.FormaAddIn = window.FormIt.FormaAddIn || {};
+
+window.FormIt.FormaAddIn.SaveCurrentAXMtoTemp = function(bSelectedOnly) {
+    args = {
+        TestAPI: "FormIt.FormaAddIn.SaveCurrentAXMtoTemp",
+        bSelectedOnly:bSelectedOnly
+    };
+    return callAsyncAPI(args);
+};
+
+window.FormIt.FormaAddIn.DeleteTempFile = function(aPath) {
+    args = {
+        TestAPI: "FormIt.FormaAddIn.DeleteTempFile",
+        aPath:aPath
+    };
+    return callAsyncAPI(args);
+};
+
+
