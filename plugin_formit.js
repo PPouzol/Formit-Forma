da790495bb9b53ae87e0e1a9f953ca9a894f6145
pierre.pouzol@autodesk.com <pierre.pouzol@autodesk.com>
2023-03-22 11:05:08 -0400

    Updates

--- plugin_formit.js
+++ plugin_formit.js
@@ -15,6 +15,22 @@ FormitPlugin.ShowDialog = function(){
     FormIt.CreateDialogBox(JSON.stringify(dialogParams));
 }
 
+window.FormIt.FormaAddIn.SaveCurrentAXMtoTemp = function(bSelectedOnly) {
+  args = {
+      TestAPI: "FormIt.FormaAddIn.SaveCurrentAXMtoTemp",
+      OptimizeGeometries:bSelectedOnly
+  };
+  return callAsyncAPI(args);
+};
+
+window.FormIt.FormaAddIn.DeleteTempFile = function(path) {
+  args = {
+      TestAPI: "FormIt.FormaAddIn.DeleteTempFile",
+      OptimizeGeometries:path
+  };
+  return callAsyncAPI(args);
+};
+
 FormitPlugin.CloseDialog = function(){
     //close parent dialog window
     FormIt.CloseDialogBox();
